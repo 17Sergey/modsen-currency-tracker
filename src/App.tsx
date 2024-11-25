6b04690b14a925748589d0d742a3efd5e17a5a03
Sergey <sergeykachaliukdev@gmail.com>
2024-11-25 21:09:49 +0300

    setup: added configuration for webpack, react, typescript, babel, eslint, prettier, husky

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import Car from "./assets/images/car.jpg";
+import ReactIcon from "./assets/images/react.svg";
+import { Counter } from "./Counter";
+
+export const App = () => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.CUSTOM);
+
+    return (
+        <div>
+            <h1>React TypeScript Webpack project for Modsen</h1>
+            <p>JSX also works, let&apos;s insert current data: {new Date().toDateString()}</p>
+            <img src={Car} alt="" width={800} height={500} />
+            <img src={ReactIcon} alt="" width={300} height={200} />
+            <Counter />
+        </div>
+    );
+};
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mode: "production", // enable webpack optimizations
+    devtool: "source-map", // more accurate, but slower and bigger files
+};
