cc1520f857a96fd90acd78779401e3e6ca66e9e0
17Sergey <sergeykachaliukdev@gmail.com>
2024-12-02 12:19:38 +0300

    fix: fixed file extensions for build on Netlify

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
         ],
     },
     output: {
-        path: path.resolve(__dirname, "..", "./dist"),
+        path: path.resolve(__dirname, "..", "./build"),
         filename: "bundle.js",
     },
     plugins: [
