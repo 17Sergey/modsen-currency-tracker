406c3254ab07b28602b3da1ed2ebb1f6368f92a3
17Sergey <sergeykachaliukdev@gmail.com>
2024-12-03 19:00:24 +0300

    feat: add styles and aria-attributes for toggle theme components

--- src/components/Header/ToggleTheme/ToggleTheme.tsx
+++ src/components/Header/ToggleTheme/ToggleTheme.tsx
@@ -1,10 +1,9 @@
 import { useTheme } from "styled-components";
 
-import Toggler from "@assets/images/toggler.png";
 import { THEMES } from "@constants/constants.ts";
 import { useToggleTheme } from "@hooks/useToggleTheme.ts";
 
-import { StyledImg } from "./styled.ts";
+import { StyledButton, StyledCircle, StyledEllipse } from "./styled.ts";
 
 export const ToggleTheme = () => {
     const { name: themeName } = useTheme();
@@ -13,12 +12,15 @@ export const ToggleTheme = () => {
     const isDarkMode = themeName === THEMES.DARK;
 
     return (
-        <button
-            aria-pressed={isDarkMode}
+        <StyledButton
+            role="switch"
+            aria-checked={isDarkMode}
             aria-label={`Switch to ${isDarkMode ? "light" : "dark"} mode`}
             onClick={toggleTheme}
         >
-            <StyledImg src={Toggler} alt="toggle-theme" />
-        </button>
+            <StyledEllipse $isDarkMode={isDarkMode}>
+                <StyledCircle $isDarkMode={isDarkMode}></StyledCircle>
+            </StyledEllipse>
+        </StyledButton>
     );
 };
