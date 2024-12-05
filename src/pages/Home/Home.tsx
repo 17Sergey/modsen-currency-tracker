b113c9ec200ca41b1650821cec3c02f0f129b8c2
17Sergey <sergeykachaliukdev@gmail.com>
2024-12-05 20:47:31 +0300

    feat(redux): add RTK Query boilerplate

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 import LastUpdated from "@components/LastUpdated";
 import WelcomeScreen from "@components/WelcomeScreen";
 import { useAppDispatch, useAppSelector } from "@hooks/reduxHooks";
+import { useAddGoodMutation, useGetGoodsQuery } from "@store/api/currencyAPI";
 import { decrement, increment, incrementByAmount } from "@store/reducers/homePageReducer";
 
 function A() {
@@ -24,10 +27,46 @@ function A() {
     );
 }
 
+function Goods() {
+    const [count, setCount] = useState("");
+    const [counter, setCounter] = useState(0);
+
+    const { data, error } = useGetGoodsQuery({ count });
+
+    const [addGood, { isLoading }] = useAddGoodMutation();
+
+    const handleAddGood = async () => {
+        const date = new Date();
+        addGood({ id: date.getMilliseconds, title: date.getFullYear, views: date.getHours }).unwrap();
+        setCounter(10);
+    };
+
+    return (
+        <div>
+            <div>
+                <select name="" id="" onChange={(e) => setCount(e.target.value)}>
+                    <option value="">all</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+                <p>count: {counter}</p>
+                <button onClick={() => setCounter(counter)}>Increment</button>
+
+                {isLoading && <p>Add loading</p>}
+                <button onClick={handleAddGood}>Add</button>
+            </div>
+            <h1>Goods:</h1>
+            {JSON.stringify(data)}
+            {error && <p>Error: {error.toString()}</p>}
+        </div>
+    );
+}
+
 export const Home = () => (
     <>
         <WelcomeScreen />
         <LastUpdated />
         <A />
+        <Goods />
     </>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { currencyAPI } from "./api/currencyAPI.ts";
 import { homePageReducer } from "./reducers/homePageReducer.ts";
 
 export const store = configureStore({
     reducer: {
         homePage: homePageReducer,
+        [currencyAPI.reducerPath]: currencyAPI.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(currencyAPI.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
