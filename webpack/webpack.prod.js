627638cf4021169e6c0168737c6be23b3c78bb74
Sergey Kachaliuk <sergeykachaliukdev@gmail.com>
2025-01-03 15:08:37 +0300

    hotfix/deploy-env-variables (#13)
    * feature/basic UI structure (#6)
    
    * setup: (test github merge restictions)
    
    * setup: added configuration for webpack, react, typescript, babel, eslint, prettier, husky
    
    * setup: add import aliases
    
    * setup: commitlint
    
    * setup: remove commitlint temporary
    
    * setup: eslint-plugin-simple-sort
    
    * setup: eslint-airbnb rules
    
    * style: add styled components and configure template for theme
    
    * style: add colors, fonts, paddings, margins configuration
    
    * fix: make compiler see the typeof of css-theme
    
    * feat(ui): add basic structure, pages mockups, styled-components context and Layout container
    
    * style: add google fonts imports
    
    * fix: change file namings, fix typescript support for styled-components
    
    * feat(ui): add routing, fallback route, reload routing support (only dev mode). And connected routing with components
    
    * fix: add eslint ignore, improve import sort groups, inline intarfaces removed
    
    * fix: fixed file extensions for build on Netlify
    
    * fix: delete files under root/build. Fix build destination to root/dist
    
    * fix: change to react-router
    
    * feature: add node version to project configuration
    
    * fix: delete not useful return statements
    
    * feat: add netlify redirects
    
    * feat: add netlify.toml for redirection
    
    * fix: remove public/_redirects file because it's not working
    
    * fix: add empty lines at the end of files
    
    * feature/common-UI-components (#7)
    
    * feat: add header layout + theme toggling
    
    * feat: add footer layout and styles, but yet not responsive
    
    * build: add dist folder to git
    
    * fix: remove comments, add links with PATHS constant
    
    * fix: remove unneccesary files from dist and .vscode folder
    
    * fix: fix .gitignore for netlify and dist folders
    
    * feat: add favicon, add responsive footer
    
    * fix: change footer css to match figma mockup
    
    * feat: add styles and aria-attributes for toggle theme components
    
    * feat: add svgr for loading svg as components
    
    * fix: wrap header li menu items in ul instead of div
    
    * fix(header): change svg icons color depending on theme
    
    * feature(ui): add updated-at component with pulse animation
    
    * feat(ui): add Welcome screen
    
    * fix: tiny fixes for clean code
    
    * fix: remove return in components if there is no logic
    
    * feat(home-page): add redux, redux toolkit, boilerplate to test, configure folder structure
    
    * feat(redux): add RTK Query boilerplate
    
    * fix: improved theme
    
    * feat: added currency icons
    
    * feat: add currency converter
    
    * refactor: refactoring Converter and Home page components & styles
    
    * fix: trying to deploy
    
    * deploy: remove cross-env package
    
    * deploy: change dependecies
    
    * fix: change to what was before
    
    * deploy: wip
    
    * deploy: wip
    
    * deploy: fix SVG naming to resolve paths
    
    * fix: change Dotenv plugin
    
    * fix: rename BurgerIcon
    
    * fix: rename CrossIcon
    
    * fix: rename CurrencyIcon
    
    * feat: error handling in Cards section
    
    * feat: add cross-env for development
    
    * feat: add Chart library and mock data
    
    * feat: add Stocks card, button and refactor CurrencyIcon components to be common
    
    * feat: Timeline page
    
    * fix: fixed layout shift with scroll disabling
    
    * fix: disable selecting out of range days
    
    * feat: add libraries and map
    
    * feature: add bank marker and BANK_COORDINATES
    
    * fix: remove unused deps
    
    * fix: fixing UI of the whole app
    
    * feature: add redux support. caching, ui improvements
    
    * feat: add class components and Error Boundary
    
    * refactor: code refactoring in all pages
    
    * fix: bug with local storage
    
    * fix: remove unused deps
    
    * style: change Modal box color
    
    * test: jest configuration
    
    * test: utils unit tests
    
    * test!: fixed testing environment for integration tests
    
    * test: toggle theme module
    
    * refactor: fixed mentor comments
    
    * test: e2e tests for full app
    
    * deploy: try fixing env vars
    
    * deploy: fix prod config and npm command

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,9 +3,67 @@
 import Dotenv from "dotenv-webpack";
 import TerserPlugin from "terser-webpack-plugin";
 
+import CopyPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { dirname, resolve } from "path";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const config = {
     mode: "production",
     devtool: "source-map",
+    entry: resolve(__dirname, "../src/index.tsx"),
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        plugins: [
+            new TsconfigPathsPlugin({
+                extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: "asset/inline",
+            },
+            {
+                test: /\.svg$/i,
+                type: "asset",
+                resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+                use: ["@svgr/webpack"],
+            },
+        ],
+    },
+    output: {
+        path: resolve(__dirname, "../dist"),
+        filename: "bundle.js",
+    },
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
@@ -14,7 +72,14 @@ const config = {
         new Dotenv({
             systemvars: true,
         }),
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, "../src/index.html"),
+        }),
+        new CopyPlugin({
+            patterns: [{ from: "public", to: "./public" }],
+        }),
     ],
+    stats: "errors-only",
 };
 
 export default config;
